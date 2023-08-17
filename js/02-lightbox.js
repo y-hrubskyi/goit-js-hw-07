4f09d760555854c83cfb3800a3711ca47d580dcb
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-17 17:59:47 +0300

    Solving the SimpleLightbox library task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+renderGalleryMarkup(galleryItems, galleryRef);
+
+const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function renderGalleryMarkup(arr, listRef) {
+  const markup = arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+          </a>
+        </li>`
+    )
+    .join('');
+  listRef.innerHTML = markup;
+}
