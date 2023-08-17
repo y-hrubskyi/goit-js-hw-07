1e0bef6d17433664b4a6627f3025b95d63c7feb6
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-17 12:38:55 +0300

    Solving the Image gallery task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.addEventListener('click', onGalleryRefClick);
+
+renderGalleryMarkup(galleryItems, galleryRef);
+let instance = null;
+
+function renderGalleryMarkup(arr, listRef) {
+  const markup = arr
+    .map(
+      ({ preview, description }, index) =>
+        `<li class="gallery__item" data-id=${index}><img class="gallery__image gallery__link" src="${preview}" alt="${description}"></li>`
+    )
+    .join('');
+  listRef.innerHTML = markup;
+}
+
+function onGalleryRefClick(event) {
+  const { target } = event;
+  if (!target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const id = Number(target.closest('.gallery__item').dataset.id);
+  showModal(galleryItems[id]);
+}
+
+function showModal({ original, description }) {
+  instance = basicLightbox.create(
+    `<img src="${original}" alt="${description}">`
+  );
+  instance.show();
+
+  document.addEventListener('keydown', onKeyDown);
+}
+
+function onKeyDown(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', onKeyDown);
+  }
+}
